f0bc1da9f6bf14cfc1b94a92b27b465e75623c2b
Ben <battlefrontgamer@gmail.com>
2021-06-07 13:57:55 -0400

    Add comments and documentation for Problem 5. Discoveries have been made, more efficient methods have been created, and I now know tons more about lists and how to use them

--- src/main/java/org/personal/projecteuler/Problem5.java
+++ src/main/java/org/personal/projecteuler/Problem5.java
@@ -29,61 +29,66 @@ import java.util.List;
 
 /**
  * @author Benjamin Dodge
- * @problem - 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
- * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * @problem - 2520 is the smallest number that can be divided by each of the
+ * numbers from 1 to 10 without any remainder. What is the smallest positive
+ * number that is evenly divisible by all of the numbers from 1 to 20?
  */
 public class Problem5 {
-    
+
     public static void main(String[] args) {
-        System.out.println(smallestAbsoluteDivisor(20));
-        //System.out.println(sieveOfEratosthenes(20));
+        smallestAbsoluteDivisor(20);
     }
-    
-   public static List<Integer> sieveOfEratosthenes(int n) {
-        boolean prime[] = new boolean[n + 1];
-        Arrays.fill(prime, true);
-        for (int p = 2; p * p <= n; p++) {
+
+    /**
+     * Millennia-old algorithm for finding all prime numbers below a limit by marking all composite numbers and their multiples, leaving only primes unmarked
+     * See Wikipedia here: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+     * 
+     * Note: Obviously not my own idea, but my own implementation of the idea
+     * 
+     * @param n - number used to find all primes below it 
+     * @return list of all prime numbers less than the input n 
+     */
+    public static List<Integer> sieveOfEratosthenes(int n) {
+        boolean prime[] = new boolean[n + 1];       //Create a new boolean array with indexes from 0 to the number 
+        Arrays.fill(prime, true);                   //Set these value all to true 
+        for (int p = 2; p * p <= n; p++) {          //Square root rule: there are no prime number factors of a given number greater than the square root of that number. Used to reduce runtime.
             if (prime[p]) {
-                for (int i = p * 2; i <= n; i += p) {
-                    prime[i] = false;
+                for (int i = p * 2; i <= n; i += p) {   //Remove all multiples of each composite index - every non-prime multiple of a prime number will be removed, as well as all multiples of a given composite number
+                    prime[i] = false;               //Setting non-prime index numbers as false allows them to be removed later
                 }
             }
         }
-        List<Integer> primeNumbers = new LinkedList<>();
-        for (int i = 2; i <= n; i++) {
-            if (prime[i]) {
+        List<Integer> primeNumbers = new LinkedList<>();    //Where we will put the prime numbers
+        for (int i = 2; i <= n; i++) {      //Iterate through list
+            if (prime[i]) {                 //If the index value is still true, it's prime, so add it to the list
                 primeNumbers.add(i);
             }
         }
-        return primeNumbers;
+        return primeNumbers;    //Return the list 
     }
-    
-    public static double smallestAbsoluteDivisor(int limit) {
-        int finalNumber = 1; 
-        int counter = 0; 
-        boolean check = true; 
-        int searchLimit = (int) Math.sqrt(limit);
-        List<Integer> primeList = sieveOfEratosthenes(limit);
-        while(primeList.get(counter) <= limit && counter <= primeList.size() - 1) {
-            long exponent = 1;
-            if(check) {
-                if(primeList.get(counter) <= searchLimit) 
-                {
-                    exponent = (int) Math.floor(Math.log(limit) / Math.log(primeList.get(counter)));
-                }
-                else {
-                    check = false; 
-                }
-            }
+
+    /**
+     * Iterating through a list of prime factors of a number, this program finds their corresponding exponents to find the minimum possible number divisible by all positive integers less than the limit
+     * 
+     * @reasoning - First, the lowest possible number (lpn) divisible by all integers less than a given limit must be divisible by the maximum power of a prime factor of the numbers in the sequence. 
+     *      For example, for the sequence 1-8, the lpn must be divisible by 8, or 2^3. However, in order for this number to be the lowest possible, it can only have an exponent of 3 for 2:
+     *      any greater, and the number has an unneeded factor of 2. To find the appropriate exponent, we first seek to find the exponent that results in a given prime number p equaling the limit.
+     *      We have p ^ x power = limit. We can use logarithmic properties to convert the equation into xlog(p) = log(limit). (Notice the application of the power rule). Continuing onward, 
+     *      we have x = log(limit)/log(p). Since we want an integer factor, we want an integer exponent, so we take the floor of that expression. Every prime number, raised to that power, will 
+     *      result in the minimum possible number of a given prime factor for the product of all such numbers to be the lpn. 
+     * @evidence - For 1-10, we have the primes 2, 3, 5, and 7. We then find the largest powers of prime factors, found in 8 = 2^3 and 9 = 3^2. No other number in this sequence is composed of a prime number raised 
+     *      to a power greater than 1, so our answer is the product of 2^3, 3^2, 5, and 7, resulting in 2520, the correct answer given in the problem. 
+     */
+    //Turning the equation primeP ^ x power = limit into xlog(primeP) = log(limit), or x = log(limit)/log(primeP).
+    public static void smallestAbsoluteDivisor(int limit) {
+        int finalNumber = 1;
+        int counter = 0;
+        List<Integer> primeList = sieveOfEratosthenes(limit);   //Calling on Eratosthenes 
+        while (counter <= primeList.size() - 1) {
+            int exponent = (int) Math.floor(Math.log(limit) / Math.log(primeList.get(counter)));  //Floored because we want the nearest integer value, which will produce an integer prime factor.
             finalNumber *= Math.pow(primeList.get(counter), exponent);
-            //TODO: Figure out why this is necessary for the program to compile
-            if(counter == primeList.size() -1) {
-                break;
-            }
-            else {
-                counter += 1; 
-            }
+            counter += 1;
         }
-        return finalNumber;
+        System.out.println(finalNumber); //Doing so displays an unaltered value to the console, while returning finalNumber would display it in scientific notation. Not wrong, but I think the number looks cleaner the other way. 
     }
 }
